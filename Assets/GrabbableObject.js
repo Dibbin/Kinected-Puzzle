fae0fc39be31fb55a2d3c79666c33913081bb183
david viramontes <dviramontes@gmail.com>
2015-01-24 22:14:04 -0700

    adds GrabbleObject.js

---
+++ Assets/GrabbableObject.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+ 
+var TheSystem : Transform;
+var Distance  : float;
+var MaxDistance : float = 100;  
+
+function Update() {
+
+ var hit : RaycastHit;
+ if (Physics.Raycast (TheSystem.transform.position, TheSystem.transform.TransformDirection(Vector3.forward), hit)) {    
+    
+    if(hit.transform.gameObject.tag == "Sphere"){
+ 
+     Distance = hit.distance;
+     if (Distance < MaxDistance){
+     
+         if (Input.GetKeyDown(KeyCode.E)) {
+            // show
+         	renderer.enabled = true;
+         	Destroy (GameObject.FindWithTag("sword2"));
+         } 
+
+        if (Input.GetKeyDown(KeyCode.Backspace)) {
+        	// hide
+        	renderer.enabled = false;
+     	}    
+	}
+}
+}
+}
