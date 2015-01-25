d31bb0e64f4bbc2da03b720f667977834c166469
Sara <saralouviere@gmail.com>
2015-01-24 17:45:43 -0700

    Credits Scene

---
+++ Assets/loadCredits.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var timeToWait = 10.0; //seconds
+
+function Start () {
+
+}
+
+function Update () {
+	timeToWait -= Time.deltaTime;
+	if (timeToWait <= 0)
+	{
+		Application.LoadLevel ("credits");
+	}
+}
+
