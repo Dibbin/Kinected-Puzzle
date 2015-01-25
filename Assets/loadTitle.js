ebfc5c4eb29949a8deec225804cca0a868a3c355
Marie <marie.angoulvant@gmail.com>
2015-01-24 21:59:15 -0700

    intro sequence and title

---
+++ Assets/loadTitle.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var timeToWait = 15.0; //seconds
+
+function Start () {
+
+}
+
+function Update () {
+	timeToWait -= Time.deltaTime;
+	if (timeToWait <= 0)
+	{
+		Application.LoadLevel ("title");
+	}
+}
+
