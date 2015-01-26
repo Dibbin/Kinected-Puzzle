52e481ad87fd9c981ba8af5f17ce6ba839bd8636
joeyverbeke <joeyverbeke@gmail.com>
2015-01-25 21:17:01 -0700

    merge cleanup

---
+++ Assets/Grab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Kinect = Windows.Kinect;
+
+public class Grab : HandTracker {
+
+	public Vector3 position;
+
+	// Use this for initialization
+	/*void Start () 
+	{
+		bool grabbed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		Kinect.JointType hand = Kinect.JointType.HandRight
+		position = GetVector3FromJoint (Kinect.);
+
+		print ("pos: " + position);
+
+		//if hand was open when it came in the same XY coordiantes and is now closed
+	}*/
+
+} 
